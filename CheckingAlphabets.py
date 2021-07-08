15fe9c29215a4b3edf81b9c2c77e4ba8664ea3c7
Shivkumar Nagre <70746102+shivkumarnagre@users.noreply.github.com>
2021-07-08 10:20:16 +0530

    Create CheckingAlphabets.py
    Write a python program to check whether the given character is vowel or consonan

---
+++ CheckingAlphabets.py
@@ -0,0 +1,31 @@
+'''Write a python program to check whether the given character is vowel or consonant.
+
+Input format:
+
+The input consist of a character
+
+Output format:
+
+The output consists of a below-given string “Vowel” / “Consonant” / “Not an alphabet”
+
+Sample Input:
+
+a
+
+Sample Output:
+
+Vowel
+
+'''
+
+
+ch=input()
+vowel=['a','e','i','o','u','A','E','I','O','U']
+
+if ((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+  if ch in vowel:
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("Not an alphabet")

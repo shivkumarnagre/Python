c2b1dcba4f32da516907801c788049936b78c4b9
Shivkumar Nagre <70746102+shivkumarnagre@users.noreply.github.com>
2021-07-08 08:54:55 +0530

    Create ASCIIValues.py
    Write a program to get a number(ASCII value) as input and print its equivalent character.

---
+++ ASCIIValues.py
@@ -0,0 +1,14 @@
+'''ASCII Values - II
+Write a program to get a number(ASCII value) as input and print its equivalent character.
+
+SAMPLE INPUT:
+
+103
+
+SAMPLE OUTPUT:
+
+g'''
+
+p=int(input())
+print(chr(p))
+
